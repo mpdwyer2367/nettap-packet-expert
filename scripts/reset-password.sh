7c13fd494e5dcf5bc050226948b375e6bf8e7610
Matthew Dwyer <44508518+mpdwyer2367@users.noreply.github.com>
2026-08-21 09:06:26 -0700

    Require API proof for administrator recovery

--- scripts/reset-password.sh
+++ scripts/reset-password.sh
@@ -98,6 +98,8 @@ chmod 0600 "$bootstrap_password_file"
 rm -f "$admin_finalized_file"
 
 "${compose_local[@]}" up -d --wait open-webui
+printf '%s\n' "$password" | "${compose_local[@]}" --profile provision run --rm -T \
+  assistant-provisioner --verify-admin
 provision_assistants local
 trap - EXIT
 unset password
